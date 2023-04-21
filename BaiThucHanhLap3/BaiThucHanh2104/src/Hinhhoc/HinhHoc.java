188e21c8c86b47b9f3e90d158027dcf43cff8e73
chimsedikhuya <khangtatou@gmail.com>
2023-04-21 11:33:44 +0700

    Baithhuchanh2104

---
+++ BaiThucHanhLap3/BaiThucHanh2104/src/Hinhhoc/HinhHoc.java
@@ -0,0 +1,13 @@
+package Hinhhoc;
+
+public class HinhHoc {
+    protected String tenHinh;
+
+    public String getTenHinh() {
+        return tenHinh;
+    }
+
+    public void setTenHinh(String tenHinh) {
+        this.tenHinh = tenHinh;
+    }
+}
---
+++ BaiThucHanhLap3/BaiThucHanh2104/src/Hinhhoc/HinhTron.java
@@ -0,0 +1,13 @@
+package Hinhhoc;
+
+public class HinhTron extends HinhHoc {
+    private double banKinh;
+
+    public double getBanKinh() {
+        return banKinh;
+    }
+
+    public void setBanKinh(double banKinh) {
+        this.banKinh = banKinh;
+    }
+}
